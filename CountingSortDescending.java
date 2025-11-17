905e858b5b039f4b95a9b15c401d2166227e3d3b
Nur Arini <nurarini0302@gmail.com>
2025-11-17 08:50:40 +0700

    feat: update code

--- CountingSortDescending.java
+++ CountingSortDescending.java
@@ -1,39 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author NURARINI
+ */
 public class CountingSortDescending {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        int[] data = {7, 2, 4, 1, 6, 3, 5, 4};
-
-        System.out.println("Sebelum diurutkan:");
-        printArray(data);
-
-        countingSort(data);
-
-        System.out.println("Setelah diurutkan (Descending):");
-        printArray(data);
+      int[] data = {7,2,4,1,6,3,5,4};
+      
+      System.out.println("Sebelum di urutkan : ");
+      printArray(data);
+      
+      countingSort(data);
+      
+      System.out.println("Setelah di urutkan : ");
+      printArray(data);
     }
-
+    
     static void countingSort(int[] arr) {
         int max = arr[0];
-        for (int num : arr) {
-            if (num > max) max = num;
+        for (int num : arr){
+            if(num > max) max = num;
         }
-
+        
         int[] count = new int[max + 1];
-
-        // Hitung frekuensi
-        for (int num : arr) count[num]++;
-
+        
+        for(int num : arr) count[num]++;
+        
         int index = 0;
-
-        // Output dari terbesar ke terkecil
-        for (int i = count.length - 1; i >= 0; i--) {
+        
+        for(int i = count.length - 1; i >= 0; i--) {
             while (count[i]-- > 0) {
                 arr[index++] = i;
             }
         }
     }
-
+    
     static void printArray(int[] arr) {
         for (int n : arr) System.out.print(n + " ");
         System.out.println();
     }
+
+    
 }
--- MergeSortDescending.java
+++ MergeSortDescending.java
@@ -1,76 +1,81 @@
-public class MergeSortDescending {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
 
-    // Fungsi utama untuk menjalankan program
+/**
+ *
+ * @author NURARINI
+ */
+public class MergeSortDescending {
     public static void main(String[] args) {
         int[] data = {45, 12, 78, 34, 89, 56, 23, 90};
-
-        System.out.println("Data sebelum diurutkan:");
+        
+        System.out.println("Data sebelum di urutkan: ");
         printArray(data);
-
+        
         mergeSort(data, 0, data.length - 1);
-
-        System.out.println("\nData setelah diurutkan (terbesar ke terkecil):");
+        
+        System.out.println("\nData setelah diurutkan dari terbesar ke terkecil: ");
         printArray(data);
+        
     }
-
-    // Fungsi rekursif untuk merge sort
+    
     public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
+        if(left < right) {
             int mid = (left + right) / 2;
-
+            
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
-
+            
             merge(arr, left, mid, right);
         }
     }
-
-    // Fungsi untuk menggabungkan dua bagian array
-    public static void merge(int[] arr, int left, int mid, int right) {
+    
+    public static void merge(int[] arr, int left, int mid, int right){
         int n1 = mid - left + 1;
         int n2 = right - mid;
-
+        
         int[] L = new int[n1];
         int[] R = new int[n2];
-
-        for (int i = 0; i < n1; i++)
+        
+        for(int i = 0; i < n1; i++)
             L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
+        for(int j = 0; j < n2; j++)
             R[j] = arr[mid + 1 + j];
-
-        int i = 0, j = 0, k = left;
-
-        // Ubah tanda pembanding agar hasilnya descending
-        while (i < n1 && j < n2) {
-            if (L[i] >= R[j]) {
+        
+        int i = 0, j= 0, k = left;
+        
+        while(i < n1 && j < n2){
+            if(L[i] >= R[j]) {
                 arr[k] = L[i];
                 i++;
-            } else {
+            }else {
                 arr[k] = R[j];
                 j++;
             }
             k++;
         }
-
-        // Menyalin sisa elemen
-        while (i < n1) {
+        
+        while(i < n1){
             arr[k] = L[i];
             i++;
             k++;
         }
-
-        while (j < n2) {
+        
+        while(j < n2) {
             arr[k] = R[j];
             j++;
             k++;
         }
     }
-
-    // Fungsi untuk mencetak array
+    
     public static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+    
+    
 }
